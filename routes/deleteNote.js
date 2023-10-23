9acd994dc510e341617386088c9fbc3083a316c4
Camsca <camila_vass@hotmail.com>
2023-10-23 12:44:05 -0700

    delte requst and updtate date

--- routes/deleteNote.js
+++ routes/deleteNote.js
@@ -2,4 +2,14 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs'); 
 
-router.delete('/notes/:id', (req, res) => {});
+router.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    console.log('DELETE request for note with id: ' + id);
+    const data= JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    const newData = data.filter(note => note.id !== id);
+    fs.writeFile('./db/db.json', JSON.stringify(newData), (err) => {
+        res.json(newData);
+});
+
+});
+module.exports = router;
