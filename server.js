efcd2493100e6ae09b2b97425903cb14342109b9
Camsca <camila_vass@hotmail.com>
2023-10-22 17:26:12 -0700

    routes.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 //const api = require('');
 
-const port = 3001;
+const port = 7777;
  
 app.listen(port, () => {
-    console.log(`Server listening on the port::${port}`);
+    console.log(`Server listening on the port:${port}`);
 });
