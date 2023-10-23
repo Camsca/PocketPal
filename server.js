afa5a816be997ad782f5b13c3c7f0db437528950
Camsca <camila_vass@hotmail.com>
2023-10-22 19:54:59 -0700

    route 2 notes.html

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ const fs = require('fs');
 const uuid = require('uuid');
 
 app.use(express.json()); // Middleware for parsing JSON requests
+
 app.get('/', (req, res) => {
     res.send(__dirname + '/public/index.html');
 });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(__dirname + '/public/notes.html');
+});
+
+
+
 const port = process.env.PORT || 7777;
 app.listen(port, () => {
     console.log(`Server listening on port:${port}`);
