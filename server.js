4352f550521bc322eb78d92d59b7ab98cdfef5c0
Camsca <camila_vass@hotmail.com>
2023-10-22 19:53:47 -0700

    rout to serve html

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 const uuid = require('uuid');
 
 app.use(express.json()); // Middleware for parsing JSON requests
-
+app.get('/', (req, res) => {
+    res.send(__dirname + '/public/index.html');
+});
 const port = process.env.PORT || 7777;
 app.listen(port, () => {
     console.log(`Server listening on port:${port}`);
