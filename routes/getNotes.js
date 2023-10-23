fabb7e7dfd831056bcb6d2c3ee238668bc32f9a5
Camsca <camila_vass@hotmail.com>
2023-10-23 11:43:12 -0700

    read contents of 'db.json'&hanle potentil ERR parse data

--- routes/getNotes.js
+++ routes/getNotes.js
@@ -2,4 +2,10 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');   
 
-router.get('/notes', (req, res) => {});
+router.get('/notes', (req, res) => {
+    console.log('GET request for notes');
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
